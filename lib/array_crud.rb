1015b2382ab9184b0714732ae97f6e043b5982b6
Rickey Brown <rickeynbrown94@gmail.com>
2020-04-17 21:09:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,10 +32,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["arrays!", "yikes!"]
+  array = ["wow!", "yikes!"]
   array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array
+  array = ["cool", "arrays!"]
+  array.last
 end
