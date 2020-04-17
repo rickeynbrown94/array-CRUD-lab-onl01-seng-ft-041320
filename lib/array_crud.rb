e29d8a9a7baf28b3d31ac6b81cc494bfdba2b6b5
Rickey Brown <rickeynbrown94@gmail.com>
2020-04-17 20:47:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   []
-  add_element_to_end_of_array.unshift
+  add_element_to_end_of_array.unshift(element)
 end
 
 def add_element_to_start_of_array(array, element)
